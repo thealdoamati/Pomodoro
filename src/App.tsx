5c78cd284b2e03775fae375dbd288070b1a04988
Aldo Amati <aldilo08@hotmail.com>
2024-06-25 20:49:36 -0300

    aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { Button } from "./components/Button";
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from "./styles/themes/defaults";
-import { GlobalStyle } from "./styles/themes/global";
+import { defaultTheme } from './styles/themes/defaults'
+import { GlobalStyle } from './styles/themes/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary"/>
-        <Button variant="secondary"/>
-        <Button variant="success" />
-        <Button variant="danger" />
-        <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
 }
-
