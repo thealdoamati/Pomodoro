54acaa1d3c094ea05e3fc23eeddc793469c8cfda
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2024-06-25 19:38:22 -0300

    aulas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from "./styles/themes/defaults";
+import { GlobalStyle } from "./styles/themes/global";
+
 export function App() {
   return (
-    <div></div>
+    <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary"/>
+        <Button variant="secondary"/>
+        <Button variant="success" />
+        <Button variant="danger" />
+        <Button />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
